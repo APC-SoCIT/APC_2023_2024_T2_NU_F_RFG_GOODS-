8b5e6f7c62fd68398d9f8062ddfadb83a977c794
JaimeManuelPascua <pascuajo@student.apc.edu.ph>
2024-01-08 23:34:02 +0800

    ak47

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -33,6 +33,7 @@
             </div>
         </div>
     </div>
+    
     @include('footer.footer')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
 </body>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -89,6 +89,7 @@
                                     <div class="px-4 py-2 font-semibold pt-3">Contact No.</div>
                                     <div class="px-4 py-2 pt-3">+63 956468182</div>
                                 </div>
+                                
                                 <div class="grid grid-cols-2">
                                     <div class="px-4 py-2 font-semibold pt-3">Current Address</div>
                                     <div class="px-4 py-2 pt-3">Capulong Street, Tondo, Manila</div>
