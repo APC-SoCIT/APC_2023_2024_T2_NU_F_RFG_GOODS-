c4822f3cd01bd4f8fe904cf9e1e1605af6d327c7
JaimeManuelPascua <pascuajo@student.apc.edu.ph>
2024-01-11 21:16:53 +0800

    Ilyfrl

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,18 @@ export default {
               'midnight-green': '#1A535C',
               ...colors,
             },
+            screens: {
+                'cellphone': '400px',
+                // => @media (min-width:400px)
+                'tablet': '640px',
+                // => @media (min-width: 640px)
+          
+                'laptop': '1024px',
+                // => @media (min-width: 1024px)
+          
+                'desktop': '1280px',
+                // => @media (min-width: 1280px)
+              },
         },
     },
 
