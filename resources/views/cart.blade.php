e80ff957f48b1343be60ed343dc6de29f416c7c2
HeytchHub <151029824+HeytchHub@users.noreply.github.com>
2024-03-16 08:10:21 +0800

    Gabriel Wrialm Marqueses Tidon

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -13,9 +13,23 @@
 
   <body class="bg-gray-100">
     @include('navbar.navbar')
+ 
+    <a href="/" class="ml-48 relative mt-6 bg-white w-52 h-10 flex justify-center items-center rounded-lg drop-shadow-md cursor-pointer">
+        <div name="ribbon" class="flex font-semibold text-black text-sm items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-left" viewBox="0 0 16 16" style="vertical-align: middle;">
+                <path fill-rule="evenodd" d="M10 3.5a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 1 1 0v2A1.5 1.5 0 0 1 9.5 14h-8A1.5 1.5 0 0 1 0 12.5v-9A1.5 1.5 0 0 1 1.5 2h8A1.5 1.5 0 0 1 11 3.5v2a.5.5 0 0 1-1 0z"/>
+                <path fill-rule="evenodd" d="M4.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H14.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708z"/>
+            </svg>
+            <p class="ml-1">Continue Shopping</p>
+        </div>
+    </a>
+
+
+
+
 
     <div class="container mx-auto">
-      <div class="flex flex-col shadow-md my-10 md:flex-row">
+      <div class="flex flex-col shadow-md my-6 md:flex-row">
         <div class="md:w-3/4 bg-white px-10 py-10">
           <div class="flex justify-between border-b pb-8">
             <h1 class="font-semibold text-2xl">Shopping Cart</h1>
@@ -141,6 +155,7 @@
                     });
                     
               </script>
+
             </div>
 
           {{-- in stock layout end --}}
@@ -269,12 +284,8 @@
           </script>
 
           {{-- product loop end --}}
-
-          <a href="/" class="flex font-semibold text-black text-sm mt-10">
         
-            <svg class="fill-current mr-2 text-black w-4" viewBox="0 0 448 512"><path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z"/></svg>
-            Continue Shopping
-          </a>
+          
         </div>
 
 
@@ -393,4 +404,4 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/datepicker.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
-  </body>
+      </body>
