91fe9f676a0280aa2c87751154deac14583e61d1
HeytchHub <151029824+HeytchHub@users.noreply.github.com>
2024-03-17 21:38:02 +0800

    aye

--- resources/views/orderhistory.blade.php
+++ resources/views/orderhistory.blade.php
@@ -5,34 +5,37 @@
 
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="">
-                    <p>Your Orders</p>
+                    <p class="font-bold text-lg">Your Orders</p>
                 </div>
+            </div>
 
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="w-full">
                 @foreach ($orders as $order)
-                    <div class="w-full py-2"><p>Reference Number: {{$order->id}}</p>
-                        @php
-                            $totalPrice = 0;
-                            $itemsCount = count($orderItems[$order->id]); // Count the number of items
-                        @endphp
-                        @foreach ($orderItems[$order->id] as $item)
+                    <div class="w-full py-2 flex justify-between items-center bg-gray-100 p-10 rounded-lg mb-4">
+                        <div>
+                            <p>Reference Number: {{$order->id}}</p>
+                            @php
+                                $totalPrice = 0;
+                                $itemsCount = count($orderItems[$order->id]); // Count the number of items
+                            @endphp
+                            @foreach ($orderItems[$order->id] as $item)
                             @php
                                 $totalPrice += $item->price;
                             @endphp
                         @endforeach
-
-                        <p>Number of Items: {{$itemsCount}}</p>
-                        <p>Total Price: ₱{{$totalPrice}}</p>
-                        <a href="/profile/orders/history/{{$order->id}}" class="text-orange-600">More Details</a>
-                        @if($order->status=='completed')
-                        <a href="/profile/orders/history/{{$order->id}}">Rate Now!</a>
-                        @endif
-
+                            <p>Number of Items: {{$itemsCount}}</p>
+                            <p>Total Price: ₱{{$totalPrice}}</p>
+                        </div>
+                        <div class="text-right">
+                            <p class="text-right">Date Ordered: {{$order->created_at}}</p>
+                            <a href="/profile/orders/history/{{$order->id}}" class="text-orange-600 text-right">More Details</a>
+                            @if($order->status=='completed')
+                                <a href="/profile/orders/history/{{$order->id}}">Rate Now!</a>
+                            @endif
+                        </div>
                     </div>
                 @endforeach
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
 
                 </div>
             </div>
